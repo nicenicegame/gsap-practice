71cc418b3d801ceb64869142acd7268d9e1b8290
Nice <nicenicegame@gmail.com>
2021-01-02 20:28:34 +0700

    Add navbar toggle

--- app.js
+++ app.js
@@ -57,16 +57,13 @@ function animateSlides() {
     });
 }
 
-let mouse = document.querySelector(".cursor");
-let mouseText = mouse.querySelector("span");
+const mouse = document.querySelector(".cursor");
+const mouseText = mouse.querySelector("span");
+const burger = document.querySelector(".burger");
 
 function cursor(e) {
     mouse.style.top = e.pageY + "px";
     mouse.style.left = e.pageX + "px";
-
-    if (window.screenY > 0) {
-        mouse.classList.add("locked");
-    }
 }
 
 function activeCursor(e) {
@@ -87,6 +84,25 @@ function activeCursor(e) {
     }
 }
 
+function navToggle(e) {
+    if (!e.target.classList.contains("active")) {
+        e.target.classList.add("active");
+        gsap.to(".line1", 0.5, { rotate: "45", y: 5, background: "black" });
+        gsap.to(".line2", 0.5, { rotate: "-45", y: -5, background: "black" });
+        gsap.to("#logo", 1, { color: "black" });
+        gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -100%)" });
+        document.body.classList.add("hide");
+    } else {
+        e.target.classList.remove("active");
+        gsap.to(".line1", 0.5, { rotate: "0", y: 0, background: "white" });
+        gsap.to(".line2", 0.5, { rotate: "0", y: 0, background: "white" });
+        gsap.to("#logo", 1, { color: "white" });
+        gsap.to(".nav-bar", 1, { clipPath: "circle(50px at 100% -100%)" });
+        document.body.classList.remove("hide");
+    }
+}
+
+burger.addEventListener("click", navToggle);
 window.addEventListener("mousemove", cursor);
 window.addEventListener("mouseover", activeCursor);
 
